3a5f6c79147d0144a8615efc7bc259bfc55e0293
somename <someemail>
2016-09-22 15:11:54 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -1,12 +1,22 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class TestBowling {
 
 	@Test
 	public void testaddFrame() {
-		fail("Not yet implemented");
+		Frame frame = null;
+		frame.setFirstThrow(4);
+		frame.setSecondThrow(5);
+		
+		List<Frame> frames = new ArrayList<Frame>();
+		BowlingGame.addFrame(frame);
+		int result = frame.getFirstThrow() + frame.getSecondThrow();
+		assertEquals("Sudokus length is OK", 9, result);	
 	}
 
 }
