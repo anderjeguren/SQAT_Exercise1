99f27af437e63605cbd93e78cfd7a68d0098a41b
somename <someemail>
2016-09-22 16:41:56 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -27,11 +27,25 @@ public class TestBowling {
 		
 		Frame frame = new Frame(1,4);
 		Frame frame2 = new Frame(4,5);
-		Frame frame3 = new Frame(1,5);
+		Frame frame3 = new Frame(6,4);
+		Frame frame4 = new Frame(5,5);
+		Frame frame5 = new Frame(10,0);
+		Frame frame6 = new Frame(0,1);
+		Frame frame7 = new Frame(7,3);
+		Frame frame8 = new Frame(6,4);
+		Frame frame9 = new Frame(10,0);
+		Frame frame10 = new Frame(2,8);
 		
 		bg.addFrame(frame);
 		bg.addFrame(frame2);
 		bg.addFrame(frame3);
+		bg.addFrame(frame4);
+		bg.addFrame(frame5);
+		bg.addFrame(frame6);
+		bg.addFrame(frame7);
+		bg.addFrame(frame8);
+		bg.addFrame(frame9);
+		bg.addFrame(frame10);
 		
 		bg.setBonus(6, 2);
 		
@@ -45,13 +59,27 @@ public class TestBowling {
 		
 		BowlingGame bg = new BowlingGame();
 		
-		Frame frame = new Frame(4,5);
-		Frame frame2 = new Frame(10,0);
-		Frame frame3 = new Frame(1,5);
+		Frame frame = new Frame(1,4);
+		Frame frame2 = new Frame(4,5);
+		Frame frame3 = new Frame(6,4);
+		Frame frame4 = new Frame(5,5);
+		Frame frame5 = new Frame(10,0);
+		Frame frame6 = new Frame(0,1);
+		Frame frame7 = new Frame(7,3);
+		Frame frame8 = new Frame(6,4);
+		Frame frame9 = new Frame(10,0);
+		Frame frame10 = new Frame(2,8);
 		
 		bg.addFrame(frame);
 		bg.addFrame(frame2);
 		bg.addFrame(frame3);
+		bg.addFrame(frame4);
+		bg.addFrame(frame5);
+		bg.addFrame(frame6);
+		bg.addFrame(frame7);
+		bg.addFrame(frame8);
+		bg.addFrame(frame9);
+		bg.addFrame(frame10);
 		
 		int result = bg.score();
 		
