7c84a8a8da9cd447bcd31d12cb6d86bee8902f63
somename <someemail>
2016-09-22 16:13:45 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -17,19 +17,20 @@ public class Frame {
 
 	//returns the score of a single frame
 	public int score(){
-		//to be implemented
 		return firstThrow + secondThrow;
 	}
 
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
-		//to be implemented
+		if (firstThrow == 10)
+				return true;
 		return false;
 	}
 	
 	//return whether a frame is a spare or not
 	public boolean isSpare(){
-		//to be implemented
+		if (firstThrow + secondThrow == 10)
+			return true;
 		return false;
 	}
 }
