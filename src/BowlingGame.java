1036ba0e91c228165ef809305babd98ba464e39b
somename <someemail>
2016-09-22 15:33:46 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -29,7 +29,7 @@ public class BowlingGame {
 	public int score(){
 		int score = 0;
 		Frame frameResult;
-		for(int i=0; i<3; i++){
+		for(int i=0; i<frames.size(); i++){
 			frameResult = getFrame();
 			score += frameResult.getFirstThrow() + frameResult.getSecondThrow();		
 	}
